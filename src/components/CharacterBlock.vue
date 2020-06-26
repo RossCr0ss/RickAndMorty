7f95f6cf7ff6a7565d7bddc0c77f3cdf1118d9bf
Rostyslav <sobechko@newveer.com>
2020-06-26 13:14:08 +0300

    complete app

---
+++ src/components/CharacterBlock.vue
@@ -0,0 +1,48 @@
+<template>
+  <div 
+    class="character-block"
+    @click="detailedInfo"
+  >
+      <div class="character-block__image">
+          <img :src="character.image" :alt="character.name">
+      </div>
+      <div class="character-block__info">
+        <span 
+            class="title"
+        >
+            {{character.name}}
+        </span>
+        <span>
+            {{character.status}} - {{character.species}}
+        </span>
+        <span>Last know location: {{character.location.name}}</span>
+      </div>
+      
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'character-block',
+    props: {
+        character: {
+            type: Object,
+            required: true
+        }
+    },
+    data() {
+        return {
+            title: 'Character Block'
+        }
+    },
+    methods: {
+        detailedInfo() {
+            this.$router.push({name: 'CharacterDetails', params: {id: this.character.id}})
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,53 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axionInstance from '@/api/index'
+import { CHARACTERS_BY_PAGE, CHARACTERS_BY_ID } from '@/api/routes'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    characters: {},
+    pages: 0
   },
   mutations: {
+    setCharacters(state, {page, characters}) {
+      state.characters[page] = characters;
+    },
+    setPages(state, pages) {
+      state.pages = pages
+    }
   },
   actions: {
+    fetchCharacters({state, commit}, page) {
+      const pageCharacters = state.characters[page];
+      if (pageCharacters) {
+        return Promise.resolve(pageCharacters)
+      }
+      return axionInstance.get(CHARACTERS_BY_PAGE(page))
+        .then(({data}) => {
+          const {info, results} = data
+          commit('setCharacters', {page, characters: results})
+          commit('setPages', info.pages)
+        })
+        .catch(err => console.log(err)
+        )
+    },
+    fetchSingleCharacter({state}, id) {
+      console.log(state)
+      return axionInstance.get(CHARACTERS_BY_ID(id))
+    }
   },
-  modules: {
+  getters: {
+    getCharacterById: (state) => ({id, page}) => {
+      const pageCharacters = state.characters[page];
+      if (pageCharacters) {
+        return pageCharacters.find(character => character.id === id)
+      } return null
+    },
+    getCharactersByPage: (state) => (page) => {
+      const pageCharacters = state.characters[page];
+      return pageCharacters
+    }
   }
 })
